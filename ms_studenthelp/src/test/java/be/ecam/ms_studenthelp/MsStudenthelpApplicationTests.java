8c458c5ce87e89227c9f2af2163f49cd97192015
Mezray <18051@ecam.be>
2022-11-18 09:15:34 +0100

    (#2)UnitTest Check DB Status, UnitTest Check POST Thread and GET Thread operation : Working

--- ms_studenthelp/src/test/java/be/ecam/ms_studenthelp/MsStudenthelpApplicationTests.java
+++ ms_studenthelp/src/test/java/be/ecam/ms_studenthelp/MsStudenthelpApplicationTests.java
@@ -1,6 +1,18 @@
 package be.ecam.ms_studenthelp;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import be.ecam.ms_studenthelp.Object.Post;
 import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,45 +20,67 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.json.JSONObject;
+import org.springframework.http.*;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class MsStudenthelpApplicationTests{
+
+	@Autowired
+	private MockMvc mockMvc;
 	@Autowired
 	private TestRestTemplate restTemplate;
-	/*
-	localhost:8080/posts/2e7e65f5-8374-4c1f-902f-66516f631f44
-	*/
+
 	@Test
-	public void UnitTestCategoriesContent() throws Exception {
-		String response = this.restTemplate.getForObject("http://localhost:" + 8080 + "/categories", String.class);
-		System.out.println((response));
-		assertNotEquals(response,"[]"); //Acceder a response et trouver si error existe
-
+	public void UnitTestCheckDBStatus() throws Exception {
+		mockMvc.perform(get("http://localhost:8080/categories"))
+				.andExpect(status().isOk());
 	}
-
-
 	@Test
 	public void UnitTestThreadsCreateContent() throws Exception {
+		/*
+		Création et envoie d'un Thread
+		 */
 		String jos = "{\"title\":\"Test thread\",\"category\":\"Math\",\"tags\":[],\"firstPost\":{\"authorId\":\"d66b3f8c-2271-4afb-a348-e370effff\",\"content\":\"Fisrt postman\"}}";
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> entity = new HttpEntity<String>(jos.toString(),headers);
 		this.restTemplate.postForObject("http://localhost:" + 8080 + "/threads",entity, String.class);
-		assertEquals(restTemplate.getForObject("http://localhost:" + 8080 + "/threads/3086ac10-4dae-4b38-a793-c462577ed200",String.class),entity); //Problème de récupération de trop => utiliser fonction dispo code ?
+		/*
+		1. Demander Récuperer ID
+		2. Demander ou mise en forme et possibilité récuperer pour comparer résultat
+		*/
+		this.mockMvc.perform(get("http://localhost:" + 8080 + "/threads"+ "/4186627e-aa7e-4ee9-b303-9d06d8db44be"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.title", is("Test thread")));
+		/*.andExpect(content().json("{\"id\":\"4186627e-aa7e-4ee9-b303-9d06d8db44be\",\"title\":\"Test thread\",\"answered\":false,\"category\":\"Math\",\"datePosted\":\"2022-11-15T19:34:42\",\"dateModified\":\"2022-11-15T19:34:42\",\"firstPost\":\"7a7b64ba-f49b-476c-9fb6-7a887ad1cee6\",\"tags\":[]}\"")));
+	*/};
 	}
 
-}
+
+
 
