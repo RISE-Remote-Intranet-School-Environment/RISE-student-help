7fe980bf021e4d4d6e1607ee15087bf7d891c945
Mezray <18051@ecam.be>
2022-11-18 22:08:19 +0100

    #2 UnitTest Posts (GET PATCH PUT DELETE) finished

---
+++ ms_studenthelp/src/test/java/be/ecam/ms_studenthelp/PostUnitTest/UnitTestGetPostContent.java
@@ -0,0 +1,32 @@
+package be.ecam.ms_studenthelp.PostUnitTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class UnitTestGetPostContent {
+    private final int port = 8080;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void unitTestGetPostContent() throws Exception {
+        String firstPost = CreateThreadTestForPosts.createThreadTestForPosts(mockMvc, port);
+
+        this.mockMvc.perform(get("http://localhost:" + port + "/posts/" + firstPost))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", is("Test post")));
+    }
+}
+
+
