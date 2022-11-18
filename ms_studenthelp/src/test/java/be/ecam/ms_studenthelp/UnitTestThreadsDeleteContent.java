8c5c56474b1c96c773069a5abe9c12bf859859da
Rayhan <rmezghani@gmail.com>
2022-11-18 15:58:39 +0100

    #2 UnitTest using API (DB + Threads) finished.

---
+++ ms_studenthelp/src/test/java/be/ecam/ms_studenthelp/UnitTestThreadsDeleteContent.java
@@ -0,0 +1,33 @@
+package be.ecam.ms_studenthelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class UnitTestThreadsDeleteContent {
+	private final int port = 8080;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void unitTestThreadsDeleteContent() throws Exception {
+		String id = CreateThreadTest.createThreadTest(mockMvc, port);
+
+		this.mockMvc.perform(delete("http://localhost:" + port + "/threads/" + id))
+				.andExpect(status().isOk());
+		this.mockMvc.perform(get("http://localhost:" + port + "/threads/" + id))
+				.andExpect(status().isNotFound());
+	}
+
+}
+
+
