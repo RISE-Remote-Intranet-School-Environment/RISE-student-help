633af3eebce6c9eb08265b76e53e1570d90bc7fa
zhao-yiming <zhao.yiming423@gmail.com>
2022-12-01 22:50:04 +0100

    #2 UnitTest Reaction (delete reaction)

--- ms_studenthelp/src/test/java/be/ecam/ms_studenthelp/ReactionUnitTest/UnitTestDeleteReaction.java
+++ ms_studenthelp/src/test/java/be/ecam/ms_studenthelp/ReactionUnitTest/UnitTestDeleteReaction.java
@@ -8,37 +8,47 @@ import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-class UnitTestGetReaction {
+class UnitTestDeleteReaction {
     private final int port = 8080;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void unitTestGetReaction() throws Exception {
+    public void unitTestDeleteReaction() throws Exception {
         String IdPost = CreateThreadTestForPosts.createThreadTestForPosts(mockMvc, port);
 
+        String json = "{\"value\":-1,\"authorId\":\"d66b3f8c-2271-4afb-a348-e370ef9990\"}";
+        this.mockMvc.perform((put("http://localhost:" + port + "/posts/" + IdPost + "/reactions")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)));
 
 
-        MvcResult answer = mockMvc.perform(get("http://localhost:" + port + "/posts/" + IdPost + "/reactions"))
+        this.mockMvc.perform(delete("http://localhost:" + port + "/posts/" + IdPost + "/reactions"))
+                .andExpect(status().isOk());
+
+        MvcResult result = this.mockMvc.perform(get("http://localhost:" + port + "/posts/" + IdPost + "/reactions"))
                 .andReturn();
 
-
-        MockHttpServletResponse response = answer.getResponse();
+        MockHttpServletResponse response = result.getResponse();
         JsonParser springParser = JsonParserFactory.getJsonParser();
-        List<Object> responseList = springParser.parseList(response.getContentAsString());
+        List<Object> answer = springParser.parseList(response.getContentAsString());
 
-        Assert.assertEquals((int) ((Map<String, Object>) responseList.get(0)).get("value"),0);
+        List<String> responseEmptyList = Collections.emptyList();
+        Assert.assertEquals(answer, responseEmptyList);
     }
 }
 
